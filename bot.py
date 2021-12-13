955d1429f78ccb7cff16152f66d6bc820fa172b9
Taifin <tralllgm@gmail.com>
2021-12-13 21:51:56 +0300

    Added ordering support, budget features

--- bot.py
+++ bot.py
@@ -34,6 +34,18 @@ def parse_date(date):
             pass
 
 
+def reset_budget(username):
+    if datetime.datetime.now().day == 1:
+        database.set_all_users("spent", username, 0)
+
+
+def get_budget_and_spent(username):
+    planned = database.lookup_all_users("budget", username).fetched_info[0][0]
+    spent = database.lookup_all_users("spent", username).fetched_info[0][0]
+
+    return planned, spent
+
+
 def bot_check_stop_in_lookup(line, update: Update, context: CallbackContext):
     if "urfin_end" in line.lower():
         context.bot.send_message(chat_id=update.effective_chat.id, text="Stop lookup!")
@@ -50,29 +62,59 @@ def bot_check_stop_in_add(line, update: Update, context: CallbackContext):
         return False
 
 
+def bot_set_budget(update: Update, context: CallbackContext):
+    amount = update.message.text.split()[1]
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Got it, updating your budget!")
+    try:
+        database.set_all_users("budget", update.effective_user.username.lower().lower(), amount)
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Done! Your monthly budget is now {0}.".format(amount))
+    except database.psycopg2.Error:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, there's something wrong. :(")
+
+
 def bot_category_lookup(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Please, enter the category you want to get "
-                                                                    "information on.")
+                                                                    "information on. You can specify the order of "
+                                                                    "information by passing column name after "
+                                                                    "the month (e.g. food amount).\n\n"
+                                                                    "COLUMNS AVAILABLE: amount, day, user_time")
     return GET_CATEGORY
 
 
 def bot_day_lookup(update: Update, context: CallbackContext):  # return to user info from particular day
-    # TODO: add option to merge data into a table
     context.bot.send_message(chat_id=update.effective_chat.id, text="Please, enter the date you want to get "
                                                                     "information on. You can enter the date in two "
-                                                                    "ways: a single or two digits meaning month"
-                                                                    " or full date, including year, "
+                                                                    "ways: a single or two digits meaning day of "
+                                                                    "current month or full date, including year, "
                                                                     "month and day (please, mind the separators, "
-                                                                    "such as ',', '.', ' ', '/', '\\', '-').")
+                                                                    "such as ',', '.', ' ', '/', '\\', '-'). You can "
+                                                                    "also specify the order of "
+                                                                    "information by passing column name after "
+                                                                    "the month (e.g. 2021-12-12 amount).\n\n"
+                                                                    "COLUMNS AVAILABLE: amount, type, user_time")
 
     return GET_DAY_LOOKUP
 
 
 def bot_month_lookup(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Please, enter the month you want to get"
-                                                                    "information on.")
+                                                                    "information on. You can specify the order of "
+                                                                    "information by passing column name after "
+                                                                    "the month (e.g. 12 amount).\n\n"
+                                                                    "COLUMNS AVAILABLE: amount, type, day, user_time")
 
     return GET_MONTH_LOOKUP
+
+
+def bot_check_budget_and_left(update: Update, context: CallbackContext):
+    reset_budget(update.effective_user.username.lower())
+
+    planned, spent = get_budget_and_spent(update.effective_user.username.lower())
+
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Your budget is {0} and you've spent {1} (delta is {2}).".format(planned, spent,
+                                                                                                   planned - spent))
 
 
 def bot_categorylookup_receive_category(update: Update, context: CallbackContext):
@@ -81,8 +123,15 @@ def bot_categorylookup_receive_category(update: Update, context: CallbackContext
     if bot_check_stop_in_lookup(user_data, update, context):
         return ConversationHandler.END
 
-    if user_data == "bot_cats":
-        data = database.user_help_categories(update.message.from_user.username).fetched_info
+    user_data = user_data.split()
+    order = "amount"
+    try:
+        order = user_data[1]
+    except IndexError:
+        pass
+
+    if user_data[0] == "bot_cats":
+        data = database.user_help_categories(update.message.from_user.username.lower()).fetched_info
         response = ''
         for row in data:
             response += "{0}\n".format(row[0])
@@ -90,7 +139,8 @@ def bot_categorylookup_receive_category(update: Update, context: CallbackContext
                                  text="Here are all the categories that you've logged:\n" + response)
         return GET_CATEGORY
 
-    data = database.lookup(update.message.from_user.username, "type", user_data.lower()).fetched_info
+    data = database.lookup(update.message.from_user.username.lower(), "type",
+                           user_data[0].lower(), order, to_ret="day").fetched_info
 
     if not data:
         context.bot.send_message(chat_id=update.effective_chat.id,
@@ -102,19 +152,27 @@ def bot_categorylookup_receive_category(update: Update, context: CallbackContext
     else:
         response = ''
         for row in data:
-            response += 'Spent {0} on {1} at {2}, your comment: {3}\n'.format(row[0], row[1],
-                                                                              row[2].strftime("%I:%M:%S"), row[3])
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Here's what I found!:\n" + response)
-            return ConversationHandler.END
+            response += 'Spent {0} at {1}, your comment: {2}\n'.format(row[0],
+                                                                       row[2].strftime("%Y.%m.%d"), row[3])
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Here's what I found!:\n" + response)
+        return ConversationHandler.END
 
 
 def bot_daylookup_receive_date(update: Update, context: CallbackContext):
-    user_data = parse_date(update.message.text)
+    user_data = update.message.text
 
     if bot_check_stop_in_lookup(user_data, update, context):
         return ConversationHandler.END
 
-    data = database.lookup(update.message.from_user.username, "day", user_data).fetched_info
+    user_data = user_data.split()
+    order = "amount"
+    try:
+        order = user_data[1]
+    except IndexError:
+        pass
+
+    data = database.lookup(update.message.from_user.username.lower(), "day", parse_date(user_data[0]),
+                           order).fetched_info
 
     if not data:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Hm... It seems that there's no data from "
@@ -136,7 +194,7 @@ def bot_daylookup_receive_date(update: Update, context: CallbackContext):
 
         response = "```\n|{0}|{1}|{2}|{3}|\n".format("Amount".center(amount_size, '-'),
                                                      "Category".center(category_size, '-'),
-                                                     "Date".center(date_size, '-'), "Comment".center(comment_size, '-'))
+                                                     "Time".center(date_size, '-'), "Comment".center(comment_size, '-'))
 
         for row in data:
             response += "|{0}|{1}|{2}|{3}|\n".format(row[0].center(amount_size), row[1].center(category_size),
@@ -147,13 +205,21 @@ def bot_daylookup_receive_date(update: Update, context: CallbackContext):
         return ConversationHandler.END
 
 
-def bot_monthlookup_receive_month(update: Update, context: CallbackContext):  # TODO: some sorting?
+def bot_monthlookup_receive_month(update: Update, context: CallbackContext):
     user_data = update.message.text
 
     if bot_check_stop_in_lookup(user_data, update, context):
         return ConversationHandler.END
 
-    data = database.lookup_month(update.message.from_user.username, "EXTRACT(MONTH FROM day)", user_data).fetched_info
+    user_data = user_data.split()
+    order = "day"
+    try:
+        order = user_data[1]
+    except IndexError:
+        pass
+
+    data = database.lookup_month(update.message.from_user.username.lower(), "EXTRACT(MONTH FROM day)", user_data[0],
+                                 order).fetched_info
 
     if not data:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Hm... It seems that there's no data from "
@@ -172,18 +238,19 @@ def bot_monthlookup_receive_month(update: Update, context: CallbackContext):  # 
             for j in range(len(data[i])):
                 ws.cell(i + 2, j + 1).value = data[i][j]
 
-        output_table.save(update.effective_user.username + ".xlsx")
+        output_table.save(update.effective_user.username.lower() + ".xlsx")
         context.bot.send_message(chat_id=update.effective_chat.id, text="Your monthly table is ready! Sending...")
         context.bot.send_document(chat_id=update.effective_chat.id,
-                                  document=open(update.effective_user.username + ".xlsx", "rb"),
-                                  filename=update.effective_chat.username + '.xlsx')
-        os.remove(update.effective_user.username + ".xlsx")
+                                  document=open(update.effective_user.username.lower() + ".xlsx", "rb"),
+                                  filename=update.effective_chat.username.lower() + '.xlsx')
+        os.remove(update.effective_user.username.lower() + ".xlsx")
 
 
 def bot_start(update: Update, context: CallbackContext):
+    reset_budget(update.effective_user.username.lower())
     context.bot.send_message(chat_id=update.effective_chat.id, text="Checking if there's already a table for you.")
     try:
-        operation = database.init_new_user(update.effective_user.username)
+        operation = database.init_new_user(update.effective_user.username.lower())
         context.bot.send_message(chat_id=update.effective_chat.id, text=operation.message)  # Created/exists
     except database.psycopg2.Error:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, something went wrong")
@@ -192,6 +259,10 @@ def bot_start(update: Update, context: CallbackContext):
 def bot_help(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Allowed commands:\n"
                                                                     "/start - use this command if you are a new user\n"
+                                                                    "/set_budget [AMOUNT] - set your monthly budget and"
+                                                                    " the bot will track amount of money you can spend"
+                                                                    " (note: this is inline command)\n"
+                                                                    "/check_budget - check your current budget\n"
                                                                     "/add - initiate process of adding new "
                                                                     "transaction\n"
                                                                     "/add_inline - initiate process of adding new"
@@ -224,6 +295,8 @@ def bot_addhelp(update: Update, context: CallbackContext):
 
 
 def bot_add_inline(update: Update, context: CallbackContext):
+    reset_budget(update.effective_user.username.lower())
+
     msg = update.message.text.split('  ')
     try:
         amount = msg[1]
@@ -237,12 +310,18 @@ def bot_add_inline(update: Update, context: CallbackContext):
             comment = msg[4]
         else:
             comment = ""
-        day = str(now.year) + str(now.month) + str(now.month)
+        day = str(now.year) + str(now.month) + str(now.day)
 
         context.bot.send_message(chat_id=update.effective_chat.id, text="Trying to add your record into database.")
         try:
-            database.add(update.effective_user.username, amount, transaction_type, day, user_time, comment)
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Done!")
+            database.add(update.effective_user.username.lower(), amount, transaction_type, day, user_time, comment)
+
+            database.update_spent(update.effective_user.username.lower(), "EXTRACT(MONTH FROM day)", now.month)
+            planned, spent = get_budget_and_spent(update.effective_user.username.lower())
+            left_amount = planned - spent if spent < planned else 0
+
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text="Done! You have {0} more to spend this month!".format(left_amount))
         except database.psycopg2.Error:
             context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, something went wrong. :(")
     except IndexError:
@@ -251,6 +330,8 @@ def bot_add_inline(update: Update, context: CallbackContext):
 
 
 def bot_add(update: Update, context: CallbackContext):
+    reset_budget(update.effective_user.username.lower())
+
     context.bot.send_message(chat_id=update.effective_chat.id, text="Arrrr, you want to log some wasted money? "
                                                                     "Please tell me how much.")
 
@@ -315,12 +396,19 @@ def bot_add_receive_comment(update: Update, context: CallbackContext):
 def bot_add_insert(update: Update, context: CallbackContext):
     user = context.user_data
     now = datetime.datetime.now()
-    day = str(now.year) + str(now.month) + str(now.month)
+    day = str(now.year) + str(now.month) + str(now.day)
     user["time"] = datetime.datetime(now.year, now.month, day=now.day, hour=int(user["time"][:2]),
                                      minute=int(user["time"][3:])).strftime('%Y-%m-%d %H:%M:%S')
     try:
-        database.add(update.effective_user.username, user["amount"], user["type"], day, user["time"], user["comment"])
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Done!")
+        database.add(update.effective_user.username.lower(), user["amount"], user["type"], day, user["time"],
+                     user["comment"])
+
+        database.update_spent(update.effective_user.username.lower(), "EXTRACT(MONTH FROM day)", now.month)
+        planned, spent = get_budget_and_spent(update.effective_user.username.lower())
+        left_amount = planned - spent if spent < planned else 0
+
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Done! You have {0} more to spend this month!".format(left_amount))
     except database.psycopg2.Error:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, something went wrong. :(")
 
@@ -341,6 +429,8 @@ def bot_initialize_and_start():
     dispatcher.add_handler(CommandHandler('help', bot_help))
     dispatcher.add_handler(CommandHandler('addhelp', bot_addhelp))
     dispatcher.add_handler(CommandHandler('add_inline', bot_add_inline))
+    dispatcher.add_handler(CommandHandler('set_budget', bot_set_budget))
+    dispatcher.add_handler(CommandHandler('check_budget', bot_check_budget_and_left))
 
     add_conv = ConversationHandler(entry_points=[CommandHandler('add', bot_add)],
                                    states={
@@ -385,18 +475,6 @@ def bot_initialize_and_start():
     updater.start_polling()
     updater.idle()
 
-
-#  TODO: add buttons "Help", "Start", "Lookup", "Add"
-#       Help:
-#           addhelp, lookuphelp, help, back
-#       Start:
-#           no branches
-#       Lookup:
-#           daylookup, monthlookup, categorylookup
-#       Add:
-#           no branches
-
-# TODO: in general: add monthly budget
 
 if __name__ == '__main__':
     database.init()
